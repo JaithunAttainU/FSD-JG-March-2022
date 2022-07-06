79cf0b9c742a81debc9891deefc9ebc248b4b87f
jmahirakz <jmahirakz@paypal.com>
2022-07-06 23:26:47 +0530

    Events, DOM Intro

---
+++ Front-End/Js-WebDev/Day 1 - Events/script.js
@@ -0,0 +1,28 @@
+function btnClickHandler() {
+  console.log("Button Clicked")
+}
+
+function paraMouseEnter() {
+  console.log("Mouse Enter Para")
+}
+
+function paraMouseLeave() {
+  console.log("Mouse Leave Para")
+}
+
+const loginBtnEle = document.getElementById('loginBtn')
+console.log(loginBtnEle)
+
+// (eventName, handler/function)
+loginBtnEle.addEventListener('click', function () {
+  console.log("Login Click")
+})
+
+const btnElements = document.getElementsByClassName('btn')
+console.log(btnElements)
+
+const btnElementsByTag = document.getElementsByTagName('button')
+console.log(btnElementsByTag)
+
+
+console.log("querySelector", document.querySelector('#loginBtn'))
