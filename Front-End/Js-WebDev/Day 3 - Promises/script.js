2a955bf060e7dde86a469776e150912dd06674db
jmahirakz <jmahirakz@paypal.com>
2022-07-08 23:32:05 +0530

    DOM Manipulation, Promise Intro

---
+++ Front-End/Js-WebDev/Day 3 - Promises/script.js
@@ -0,0 +1,2 @@
+const fetchResponse = window.fetch('https://jsonplaceholder.typicode.com/todos/1')
+console.log(fetchResponse)
--- Front-End/Js-WebDev/ToDo App/script.js
+++ Front-End/Js-WebDev/ToDo App/script.js
@@ -1,15 +1,44 @@
 const addBtnEle = document.querySelector('#add-btn')
 const inputEle = document.querySelector('#task-input')
+
 const toDoList = document.querySelector('#to-do')
+// console.log(toDoList.classList)
 
 addBtnEle.addEventListener('click', () => {
   // console.log("Add btn clicked")
-  // console.log(inputEle.value)
-  const newTask = inputEle.value
 
-  console.log(toDoList)
-  const oldTasks = toDoList.innerHTML
+  // console.log(inputEle)
+  // console.log(inputEle.value) //get value from the input element
+  const newTask = inputEle.value //user input 
+
+  const oldTasks = toDoList.innerHTML //get old task
   console.log(oldTasks)
-  toDoList.innerHTML = `${oldTasks}<li>${newTask}</li>`
+
+  // toDoList.innerHTML = `${oldTasks}<li>${newTask}<button>X</button></li>` //add task tasks aling with old one
   inputEle.value = null
+
+
+  //create HtmlElemt li
+  const liEle = document.createElement('li') //<li></li>
+  liEle.innerText = newTask //<li>Task 3</li>
+
+  //create button ele
+  const deleteBtnEle = document.createElement('button') //<button></button>
+  deleteBtnEle.innerText = 'X' //<button>X</button>
+  deleteBtnEle.addEventListener('click', removeTaskHandler)
+
+  //apend button to li tag
+  liEle.append(deleteBtnEle) //<li>Task 3<button>X</button></li>
+
+  // liEle.appendChild(deleteBtnEle) //Only HTMLNodes
+  //append li tag to unordered list
+  toDoList.prepend(liEle)
 })
+
+//Get the parent of button(i.e; li tag) and then remove it
+function removeTaskHandler(event) {
+  console.log("Delete button is clicked", event)
+  event.target.parentElement.remove()
+}
+
+/**add, toggle, prepend, appendChild, before after */
