e72fbf17217ba56471ea7d334be60b10573904a0
jmahirakz <jmahirakz@paypal.com>
2022-08-26 12:21:31 +0530

    React Introduction

---
+++ Front-End/React/day-1/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement)
+root.render('HELO') //virtual DOM
